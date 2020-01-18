21502f42f58c1eebf25f2fb99d7bd2bc34b3bc34
Luke Butler <lukepbutler@gmail.com>
2020-01-18 18:15:04 -0500

    using epanet-js

--- src/utils/epanet/index.ts
+++ src/utils/epanet/index.ts
@@ -1,4 +1,4 @@
-import Module from "./output";
+import { Project, Workspace } from "epanet-js";
 import { readBinary } from "../EpanetBinary";
 import { toGeoJson } from "../EpanetToGeojson";
 
@@ -13,27 +13,44 @@ export function runEpanet(
   fromProject: string,
   openModel: (file: ModelFeatureCollection) => void
 ): void {
-  const epaNetEngine = Module();
+  // Initialise a new Workspace and Project object
+  const ws = new Workspace();
+  const model = new Project(ws);
+
+  // Write a copy of the inp file to the workspace
+  ws.writeFile("net1.inp", fromProject);
+
+  // Runs toolkit methods: EN_open, EN_solveH & EN_close
+  model.open("net1.inp", "report.rpt", "out.bin");
+  model.solveH();
+  model.saveH();
+  model.closeH();
+
+  model.close();
+
+  const resultView = ws.readFile("out.bin", "binary");
+
+  //  const epaNetEngine = Module();
   //@ts-ignore
-  epaNetEngine.onRuntimeInitialized = _ => {
-    const FS = epaNetEngine.fs;
-    FS.writeFile("/net1.inp", fromProject);
-    const t0 = performance.now();
-    console.log(epaNetEngine._epanet_run());
-    const t1 = performance.now();
-    console.log("Call to _epanet_run took " + (t1 - t0) + " milliseconds.");
+  //  epaNetEngine.onRuntimeInitialized = _ => {
+  //    const FS = epaNetEngine.fs;
+  //    FS.writeFile("/net1.inp", fromProject);
+  //    const t0 = performance.now();
+  //    console.log(epaNetEngine._epanet_run());
+  //    const t1 = performance.now();
+  //    console.log("Call to _epanet_run took " + (t1 - t0) + " milliseconds.");
 
-    const resultView = FS.readFile("/net1.bin");
+  //const resultView = FS.readFile("/net1.bin");
 
-    const t2 = performance.now();
-    const results = readBinary(resultView);
-    const t3 = performance.now();
+  const t2 = performance.now();
+  const results = readBinary(resultView);
+  const t3 = performance.now();
 
-    const geoJson = toGeoJson(fromProject, results);
-    console.log(geoJson);
-    console.log(results);
-    console.log("Call to readBinary took " + (t3 - t2) + " milliseconds.");
+  const geoJson = toGeoJson(fromProject, results);
+  console.log(geoJson);
+  console.log(results);
+  console.log("Call to readBinary took " + (t3 - t2) + " milliseconds.");
 
-    openModel(geoJson);
-  };
+  openModel(geoJson);
+  //  };
 }
