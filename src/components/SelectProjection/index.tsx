d90f7bb6baaaef033e1da709f475435ed1679223
Luke Butler <lukepbutler@gmail.com>
2020-05-25 15:45:49 -0400

    updated all versions

--- src/components/SelectProjection/index.tsx
+++ src/components/SelectProjection/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import AsyncSelect from "react-select/lib/Async";
-import { ValueType, ActionMeta, OptionsType } from "react-select/lib/types";
 import Button from "@material-ui/core/Button";
-import { proj4List } from "./epsg";
+//import { proj4List } from "./epsg";
 
 interface ProjectionOption {
   value: string;
@@ -13,7 +11,7 @@ type SelectProjectionProps = {
   onSelectProj: (proj: string) => void;
 };
 interface SelectProjectionState {
-  selectedOption: ValueType<ProjectionOption> | null;
+  //selectedOption: ValueType<ProjectionOption> | null;
 }
 
 class SelectProjection extends React.Component<
@@ -21,37 +19,42 @@ class SelectProjection extends React.Component<
   SelectProjectionState
 > {
   state: Readonly<SelectProjectionState> = {
-    selectedOption: null
+    selectedOption: null,
   };
 
-  onInputChange = (
-    inputValue: ValueType<ProjectionOption>,
-    { action }: ActionMeta
+  onInputChange = () =>
+    //inputValue: ValueType<ProjectionOption>,
+    //{ action }: ActionMeta
+    {
+      // this.setState({ selectedOption: inputValue });
+    };
+  getOptions = (
+    input: string
+
+    //callback: (
+    //c: OptionsType<any>) => void
   ) => {
-    this.setState({ selectedOption: inputValue });
-  };
-  getOptions = (input: string, callback: (c: OptionsType<any>) => void) => {
-    const searchString = input.toLowerCase().trim();
-    console.log(searchString);
-    //input.length > 0 ?
-
-    const options: ProjectionOption[] = proj4List.filter(d => {
-      return d.label.toLocaleLowerCase().match(searchString);
-    });
-
-    return callback(options.slice(0, 20));
+    //    const searchString = input.toLowerCase().trim();
+    //    console.log(searchString);
+    //    //input.length > 0 ?
+    //
+    //    const options: ProjectionOption[] = proj4List.filter(d => {
+    //      return d.label.toLocaleLowerCase().match(searchString);
+    //    });
+    //
+    //    return callback(options.slice(0, 20));
   };
 
   _submitProjection = () => {
-    if (this.state.selectedOption) {
-      // Todo, fix this us - source https://github.com/JedWatson/react-select/issues/2902
-      if (Array.isArray(this.state.selectedOption)) {
-        throw new Error(
-          "Unexpected type passed to ReactSelect onChange handler"
-        );
-      }
-      this.props.onSelectProj(this.state.selectedOption.value);
-    }
+    //if (this.state.selectedOption) {
+    //  // Todo, fix this us - source https://github.com/JedWatson/react-select/issues/2902
+    //  if (Array.isArray(this.state.selectedOption)) {
+    //    throw new Error(
+    //      "Unexpected type passed to ReactSelect onChange handler"
+    //    );
+    //  }
+    //  this.props.onSelectProj(this.state.selectedOption.value);
+    //}
   };
 
   _submitFalseProjection = () => {
@@ -61,7 +64,7 @@ class SelectProjection extends React.Component<
   };
 
   render() {
-    const { selectedOption } = this.state;
+    //const { selectedOption } = this.state;
     return (
       <>
         <p className="model-proj-subtitle">
@@ -72,29 +75,30 @@ class SelectProjection extends React.Component<
           Otherwise select 'No Basemap' to load without the background maps.
         </p>
         <p className="model-proj-subtitle">Model Projection:</p>
-        <AsyncSelect
-          autoFocus
-          placeholder="Start typing to searching projections..."
-          className="react-select-container"
-          classNamePrefix="react-select"
-          value={selectedOption}
-          onChange={this.onInputChange}
-          name="color"
-          //options={proj4List}
-          loadOptions={this.getOptions}
-        />
-        {this.state.selectedOption && (
-          <div className="btns-float-left">
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={this._submitProjection}
-            >
-              View Model
-            </Button>
-          </div>
-        )}
-
+        {
+          // <AsyncSelect
+          //   autoFocus
+          //   placeholder="Start typing to searching projections..."
+          //   className="react-select-container"
+          //   classNamePrefix="react-select"
+          //   value={selectedOption}
+          //   onChange={this.onInputChange}
+          //   name="color"
+          //   //options={proj4List}
+          //   loadOptions={this.getOptions}
+          // />
+          // this.state.selectedOption && (
+          //   <div className="btns-float-left">
+          //     <Button
+          //       variant="contained"
+          //       color="primary"
+          //       onClick={this._submitProjection}
+          //     >
+          //       View Model
+          //     </Button>
+          //   </div>
+          // )}
+        }
         <Button
           variant="contained"
           color="primary"
